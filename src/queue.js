f3353b8f0a805ebb4ba1b1624eb2023a1d390ad5
RacLad <raaclad@gmail.com>
2025-11-07 14:06:01 +0530

    getting file path

--- src/queue.js
+++ src/queue.js
@@ -2,3 +2,9 @@ import fs from  "fs";
 import path from "path";
 import chalk from "chalk";
 import { fileURLToPath } from "url";
+
+// Helper to get current file path (needed for ES modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const JOBS_FILE = path.join(__dirname, "../jobs.json");

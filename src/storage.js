1bc06fa031b7c7352f84407b8f14fd39e78d2491
RacLad <raaclad@gmail.com>
2025-11-07 15:56:10 +0530

    check for job.json or create it if not exist

--- src/storage.js
+++ src/storage.js
@@ -4,3 +4,5 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const JOBS_FILE = path.join(__dirname, "../jobs.json");
+
